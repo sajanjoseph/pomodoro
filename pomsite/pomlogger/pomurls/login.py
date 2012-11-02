2ace32aae8be0228029e9fb08686600956e8c447
Sajan Joseph <sajan@ubuntu.(none)>
2012-11-02 10:27:48 +0530

    modified register url to use RegistrationFormUniqueEmail form of djago-registration sothat two users cannot register with same email address

--- pomsite/pomlogger/pomurls/login.py
+++ pomsite/pomlogger/pomurls/login.py
@@ -1,11 +1,29 @@
 from django.conf.urls.defaults import *
 from registration.urls import urlpatterns as regurlpatterns
+from registration.forms import RegistrationFormUniqueEmail
+#from registration.views import register
+
 urlpatterns=patterns('',
 url(r'^login/$','django.contrib.auth.views.login',{'template_name':'pomlogger/mylogin.html'},name='pomlog_login'),
 url(r'^logout/$', 'pomlogger.views.logout', {}, name = 'pomlog_logout'),
 )
 
+
+
+from registration.views import register
+
+custom_reg_patterns=patterns('',
+     url(r'^register/', register, 
+      {'form_class':RegistrationFormUniqueEmail },
+      
+      name='registration_register'),
+)
+urlpatterns+=custom_reg_patterns
+
+#
 urlpatterns+=regurlpatterns
+
+
 #urlpatterns+=patterns('registration.views',
 #url(r'^register/$','register',{},name='pomlog_register'),
 #
